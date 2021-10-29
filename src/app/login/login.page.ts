22833df97e96ff40e4f8a56061066cc8e8d97580
hello99world99 <hello99world99@gmail.com>
2021-10-29 15:42:18 +0000

    Update....

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Component, OnInit } from '@angular/core';
 import { MyServiceService } from '../services/my-service.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,20 +14,25 @@ export class LoginPage implements OnInit {
   constructor(
     private mService: MyServiceService,
     private auth: AngularFireAuth,
-    private route: Router
+    private route: Router,
+    private toast: ToastController
   ) { }
 
   ngOnInit() {
   }
 
   submitForm(data: any){
-    this.auth.signInWithEmailAndPassword(data.value.email, data.value.password).then(
-      (result) => {
-        if (result){
-          this.route.navigateByUrl('/tabs');
+    if (data.value.password !== data.value.c_password){
+      this.toast.create();
+    }else{
+      this.auth.signInWithEmailAndPassword(data.value.email, data.value.password).then(
+        (result) => {
+          if (result){
+            this.route.navigateByUrl('/tabs');
+          }
         }
-      }
-    );
+      );
+    }
   }
 
 }
